ebc2af00f2fec9959d789daa8aca0e2af7011179
SonnyJack <sonny_161@hotmail.com>
2014-05-14 12:13:52 +1000

    Update EntityCharacter.h

--- EntityCharacter.h
+++ EntityCharacter.h
@@ -1,1 +1,20 @@
  
+#include "allegro5\allegro5.h"
+//#include "Entity.h"
+
+#ifndef ENTITYCHARACTER_H
+#define ENTITYCHARACTER_H
+
+class EntityCharacter
+{
+public:
+	EntityCharacter();
+	int deltaX, deltaY, segmentC, areaC;
+	void moveCharacter(int dir);
+	void update();
+private:
+	int xPos, Ypos;
+protected:
+};
+
+#endif
