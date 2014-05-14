a4832e159b058fdbcbf10977071ca16c029dd7c2
SonnyJack <sonny_161@hotmail.com>
2014-05-14 12:14:43 +1000

    Update MouseHandler.h

--- MouseHandler.h
+++ MouseHandler.h
@@ -1,1 +1,20 @@
  
+
+#include "allegro5\allegro.h"
+#ifndef MOUSEHANDLER_H
+#define MOUSEHANDLER_H
+
+
+class MouseHandler
+{
+	public:
+		MouseHandler();
+		int x, y, state;
+		void handleMouse();
+		void isClicked();
+		bool activeClick, Click, rightClick, activeRightClick;
+		ALLEGRO_MOUSE_STATE mState;
+	private:
+};
+
+#endif
