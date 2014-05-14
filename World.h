b5c542e09a5e6c6976b68b0ed70e5c3216dd0873
SonnyJack <sonny_161@hotmail.com>
2014-05-14 12:15:18 +1000

    Update World.h

--- World.h
+++ World.h
@@ -1,1 +1,18 @@
  
+
+#include "EntityCharacter.h"
+
+#ifndef WORLD_H
+class World
+{
+	public:
+		int defineSegment(int i, int j, int k, int a[]);
+		void init();
+		int drawWorld(EntityCharacter character);
+		int getTileId(int i, int j, EntityCharacter character);
+	protected:
+	private:
+		struct map;
+};
+
+#endif
