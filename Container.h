bba45e874e1fdd220321c45b05af21b3ee122f23
SonnyJack <sonny_161@hotmail.com>
2014-05-14 12:12:34 +1000

    Update Container.h

--- Container.h
+++ Container.h
@@ -1,1 +1,38 @@
  
+#ifndef CONTAINER_H
+
+#include "Item.h"
+#include <allegro5\allegro5.h>
+#include <iostream>
+#include <vector>
+
+
+#define CONTAINER_H
+
+
+class Container
+{
+public:
+	std::vector<Item *> items;
+	std::vector<int> containerSlot;
+	Container();
+	Container(int spaX, int spaY, int size, int x, int y, int rowX);	
+	int rowX, rowY, spacingX, spacingY, size, x, y, hudX, hudY, deltaItem, s, j, isItemClicked(), loopContainerSpace(), getItemId();
+	/*void update(ALLEGRO_BITMAP *item[]);
+	void modifyItem(int i, int b, int flag);
+	void assignItem(int b, int stack);*/
+	bool checkIfInBounds();
+	/*void selectItem();	
+	ALLEGRO_BITMAP *containerHUD;*/
+	void initate();
+	void selectItem();
+	void deallocate();
+	void updateDraw(ALLEGRO_BITMAP *item[]);
+	void update();
+	void setStack(int i, int b);
+	void setId(int i, int b);
+private:
+	
+protected:
+};
+#endif
