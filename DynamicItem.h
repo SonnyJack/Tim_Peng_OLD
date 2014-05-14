5e05e1ef893db3a14b4408e7c68195b1b17ae8d1
SonnyJack <sonny_161@hotmail.com>
2014-05-14 12:12:59 +1000

    Update DynamicItem.h

--- DynamicItem.h
+++ DynamicItem.h
@@ -1,1 +1,17 @@
  
+#include "Container.h"
+#include "Item.h"
+#define DYNAMICITEM_H
+#ifdef DYNAMICITEM_H
+
+class DynamicItem
+{
+	public:
+		void init();
+		Item *holdingItem;
+		bool itemSelected, activeClick, click, rightClick;
+		int accessButton, containerSlot;
+};
+
+
+#endif
